603ae050c88b727037da14eafaca065018354b56
BizmanSS <bismanlolol@gmail.com>
2024-07-08 15:47:40 +0530

    Updated perm imm form (including validation)

--- models/permanent_immigration_form.model.js
+++ models/permanent_immigration_form.model.js
@@ -2,49 +2,57 @@ const mongoose = require("mongoose");
 
 const Permanent_Immigration_Form_Schema = new mongoose.Schema(
     {
-        firstname: { type: String, required: true },
-        lastname: { type: String, required: true },
-        email: { type: String, required: true },
-        phone: { type: String, required: true },
-        maritalStatus: { type: String, required: true },
-        age: { type: String, required: true },
-        nationality: { type: String, required: true },
-        region: { type: String, required: true },
-        spouseTravelling: { type: String },
-        children: { type: String },
-        educationqualification: { type: String, required: true },
-        educationqualification1: { type: String, required: true },
-        passingyear: { type: String },
-        board: { type: String },
-        stream: { type: String },
-        country: { type: String },
-        workexperience: { type: String, required: true },
-        workexperience1: { type: String },
-        occupation: { type: String },
-        employmentHistory: { type: String },
-        workCountry: { type: String },
-        englishTest: { type: String, required: true },
-        frenchTest: { type: String, required: true },
-        englishTestType: { type: String },
-        frenchTestType: { type: String },
+        firstname: { type: String, required: true, maxlength: 50 },
+        lastname: { type: String, required: true, maxlength: 50 },
+        email: { type: String, required: true, maxlength: 320 },
+        phone: { type: String, required: true, maxlength: 50 },
+        maritalStatus: { type: String, required: true, maxlength: 50 },
+        age: { type: String, required: true, maxlength: 50 },
+        nationality: { type: String, required: true, maxlength: 50 },
+        region: { type: String, required: true, maxlength: 50 },
+        spouseTravelling: { type: String, maxlength: 50 },
+        children: { type: String, maxlength: 50 },
+        educationalQualifications: [
+            {
+                educationqualification: { type: String, maxlength: 50 },
+                passingyear: { type: String, maxlength: 50 },
+                board: { type: String, maxlength: 50 },
+                stream: { type: String, maxlength: 50 },
+                country: { type: String, maxlength: 50 },
+            }
+        ],
+        educationqualification1: { type: String, required: true, maxlength: 50 },
+        workexperiences: [
+            {
+                workexperience: { type: String, maxlength: 50 },
+                employmentHistory: { type: String, maxlength: 50 },
+                workCountry: { type: String, maxlength: 50 },
+                occupation: { type: String, maxlength: 50 },
+            }
+        ],
+        workexperience1: { type: String, maxlength: 50 },
+        englishTest: { type: String, required: true, maxlength: 50 },
+        frenchTest: { type: String, required: true, maxlength: 50 },
+        englishTestType: { type: String, maxlength: 50 },
+        frenchTestType: { type: String, maxlength: 50 },
         englishTestResult: {
-            reading: { type: String },
-            writing: { type: String },
-            listening: { type: String },
-            speaking: { type: String }
+            reading: { type: String, maxlength: 50 },
+            writing: { type: String, maxlength: 50 },
+            listening: { type: String, maxlength: 50 },
+            speaking: { type: String, maxlength: 50 }
         },
         frenchTestResult: {
-            reading: { type: String },
-            writing: { type: String },
-            listening: { type: String },
-            speaking: { type: String }
+            reading: { type: String, maxlength: 50 },
+            writing: { type: String, maxlength: 50 },
+            listening: { type: String, maxlength: 50 },
+            speaking: { type: String, maxlength: 50 }
         },
-        futureTestEnglish: { type: String },
-        futureTestFrench: { type: String},
-        certificateofNomination: { type: String, required: true },
-        canadianjobOffer: { type: String, required: true },
-        relativesinCanada: { type: String, required: true },
-        otherInformation: { type: String }
+        futureTestEnglish: { type: String, maxlength: 50 },
+        futureTestFrench: { type: String, maxlength: 50 },
+        certificateofNomination: { type: String, required: true, maxlength: 50 },
+        canadianjobOffer: { type: String, required: true, maxlength: 50 },
+        relativesinCanada: { type: String, required: true, maxlength: 50 },
+        otherInformation: { type: String, maxlength: 500 }
     },
     {
         timestamps: true
